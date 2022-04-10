f5ab08f0c4cb5769c05b6d8c9df5d6851bc9882f
6303001601 <memae.rnk@gmail.com>
2022-04-10 17:57:08 +0700

    up

---
+++ EXAM-63030016/7/authors.php
@@ -0,0 +1,8 @@
+<?php
+    $list = array(
+        array('authorID'=>1111, 'author'=>'wittaras', 'penname'=>'witty100'),
+        array('authorID'=>1112, 'author'=>'jakkarin', 'penname'=>'พ่อบ้านใจกล้า'),
+        array('authorID'=>1113, 'author'=>'worawit', 'penname'=>'obob')
+    );
+    echo json_encode($list);
+?>
